6ccca3936f0b1051bca67c66f4662fd2fb4c5c03
st451lei <staslm07@gmail.com>
2025-10-30 19:00:56 +0200

    Card, PlayerSpec, Player

---
+++ src/test/scala/de/htwg/se/uno2/model/PlayerSpec.scala
@@ -0,0 +1,34 @@
+package de.htwg.se.uno2.model
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class PlayerSpec extends AnyWordSpec {
+  
+  "A Player" should {
+    
+    "draw cards from a pile" in {
+      val player = Player("Stas", Vector.empty)
+      val pile = Vector(
+        Card(Color.Red, Rank.Number(1)),
+        Card(Color.Blue, Rank.Number(2)),
+        Card(Color.Green, Rank.Number(3))
+      )
+      
+      val (p2, rest) = player.draw(2, pile)
+      p2.size shouldBe 2
+      rest.size shouldBe 1
+    }
+    
+    "play a card by index and reduce hand size" in {
+      val player = Player("Stas", Vector(
+        Card(Color.Red, Rank.Number(5)),
+        Card(Color.Blue, Rank.Skip)
+      ))
+      val (played, p2) = player.playAt(1)
+      
+      played shouldBe Card(Color.Blue, Rank.Skip)
+      p2.size shouldBe 1
+    }
+  }
+}
