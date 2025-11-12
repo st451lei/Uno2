6d3b86ae0b6d667d3c5f7bbdcdcd502a8b7d6a53
st451lei <staslm07@gmail.com>
2025-11-12 18:15:27 +0200

    Obserable

--- src/main/scala/de/htwg/se/uno2/main.scala
+++ src/main/scala/de/htwg/se/uno2/main.scala
@@ -1,5 +1,9 @@
 package de.htwg.se.uno2
 
+import de.htwg.se.uno2.model._
+import de.htwg.se.uno2.controller.Controller
+import de.htwg.se.uno2.aview.Tui
+
 @main def runUnoTui(): Unit =
   val controller = Controller()
   val tui = Tui(controller)
