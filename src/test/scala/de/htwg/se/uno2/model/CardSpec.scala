23ec9a02e7d31df2143e5b683b843d1e4cf8a7ea
st451lei <staslm07@gmail.com>
2025-11-20 20:55:21 +0200

    Tui

--- src/test/scala/de/htwg/se/uno2/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/uno2/model/CardSpec.scala
@@ -31,5 +31,20 @@ class CardSpec extends AnyWordSpec {
       val next = Card(Color.Black, Rank.WildDrawFour)
       Card.canPlayOn(top, next, None) shouldBe true
     }
+    
+    "respect chosenColor if it is defined" in {
+      val top = Card(Color.Red, Rank.Number(5))
+      val next = Card(Color.Green, Rank.Number(9))
+      
+      Card.canPlayOn(top, next, Some(Color.Green)) shouldBe true
+      Card.canPlayOn(top, next, Some(Color.Yellow)) shouldBe false
+    }
+    
+    "not be playable if neither color nor rank match and no wild / chosen color" in {
+      val top = Card(Color.Red, Rank.Number(5))
+      val next = Card(Color.Green, Rank.Skip)
+      
+      Card.canPlayOn(top, next, None) shouldBe false
+    }
   }  
 }
