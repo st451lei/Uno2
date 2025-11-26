11b55dcef02583a3d1592456902a0b60ca965b45
st451lei <staslm07@gmail.com>
2025-11-26 22:00:46 +0200

    Template

--- src/main/scala/de/htwg/se/uno2/aview/Tui.scala
+++ src/main/scala/de/htwg/se/uno2/aview/Tui.scala
@@ -1,60 +1,54 @@
 package de.htwg.se.uno2.aview
 
 import de.htwg.se.uno2.controller.Controller
-import de.htwg.se.uno2.util.Observer
-import scala.io.StdIn.readLine
 import scala.util.Try
 
-class Tui(val controller: Controller) extends Observer:
-  controller.addObserver(this)
+class Tui(controller: Controller) extends BaseTui(controller):
+  private var exit = false
   
-  def update: Unit =
+  override def update: Unit =
     println(controller.gameStateToString)
-  
-  def run(): Unit =
-    val names = askPlayers()
-    controller.startGame(names)
-    loop()
-  
-  private def loop(): Unit =
-    print("> ")
-    val input = Option(readLine()).getOrElse("").trim
-    if controller.isAwaitingColorChoise then
-      input.toLowerCase match
-        case s if s.startsWith("color") =>
-          val parts = s.split("\\s+")
-          if parts.length >= 2 then  controller.chooseColor(parts(1))
-          else println("Verwendung: color r|y|g|b")
-          loop()
-        case "quit" =>
-          println("Spiel beendet."); ()
-        case _ =>
-          controller.chooseColor("")
-          loop()
-    else
-      input match
-        case "quit" =>
-          println("Spiel beendet."); ()
-        case "draw" =>
-          controller.drawCard; loop()
-        case s if s.startsWith("play") =>
-          val parts = s.split("\\s+")
-          if parts.length == 2 then
-            Try(parts(1).toInt).toOption match
-              case Some(index) => controller.playCard(index)
-              case None        => println("Ungültiger Index.")
-          else
-            println("Verwendung: play <index>")
-          loop()
-        case s if s.startsWith("color") =>
-          println("Jetzt keine Farbauswahl nötig.")
-          loop()
-        case _ =>
-          println("Unbekannter Befehl. (play <index>, draw, quit)")
-          loop()
+    
+  protected def shouldExit: Boolean = exit
 
-  private def askPlayers(): Seq[String] =
+  protected def handleQuit(): Unit =
+    println("Spiel beendet")
+    exit = true
+
+  protected def handleAwaitingColorInput(input: String): Unit =
+    input.toLowerCase match
+      case s if s.startsWith("color") =>
+        val parts = s.split("\\s+")
+        if parts.length >= 2 then
+          controller.chooseColor(parts(1))
+        else
+          println("Verwendung: color r|y|g|b")
+      case _ =>
+        controller.chooseColor("")
+        
+  protected def handleNormalInput(input: String): Unit =
+    input match
+      case "draw" =>
+        controller.drawCard
+
+      case s if s.startsWith("play") =>
+        val parts = s.split("\\s+")
+        if parts.length == 2 then
+          Try(parts(1).toInt).toOption match
+            case Some(index) => controller.playCard(index)
+            case None => println("Ungültiger Index.")
+        else
+          println("Verwendung: play <index>")
+
+      case s if s.startsWith("color") =>
+        print("Jetzt keine Farbauswahl nötig")
+
+      case _ =>
+        println("Unbekannter Befehl. (play <index>, draw, quit)")
+  
+
+  protected def askPlayers(): Seq[String] =
     println("Spieler eingeben, z.B. 'Ann Ben':")
-    val line  = Option(readLine()).getOrElse("")
+    val line  = Option(scala.io.StdIn.readLine()).getOrElse("")
     val names = line.split("[ ,]+").toVector.filter(_.nonEmpty)
     if names.nonEmpty then names else Vector("Player1", "Player2")
