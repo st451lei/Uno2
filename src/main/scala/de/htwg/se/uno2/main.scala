565cf637b961db4aa3a7088e9ca465b42c74a026
Nikita <yurchenkonikitos@gmail.com>
2026-01-22 22:21:45 +0100

    Docker-GUI and small Update GUI

--- src/main/scala/de/htwg/se/uno2/main.scala
+++ src/main/scala/de/htwg/se/uno2/main.scala
@@ -6,19 +6,42 @@ import net.codingwell.scalaguice.InjectorExtensions.*
 import de.htwg.se.uno2.controller.ControllerInterface
 import de.htwg.se.uno2.aview.{Tui, GUI}
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
+import scala.swing.Swing
+import java.util.concurrent.CountDownLatch
+import java.awt.event.{WindowAdapter, WindowEvent}
+
 @main def run(): Unit =
   val injector = Guice.createInjector(new Uno2Module)
-  
   val controller: ControllerInterface = injector.instance[ControllerInterface]
-  
-  val tui = new Tui(controller)
+
+  val uiMode =
+    sys.props.get("ui")
+      .orElse(sys.env.get("ui"))
+      .getOrElse("both")
+      .toLowerCase
+
   val gui = new GUI(controller)
-  
-  controller.addObserver(tui)
   controller.addObserver(gui)
 
-  Future {
-    tui.run()
+  val latch = new CountDownLatch(1)
+  Swing.onEDTWait {
+    gui.peer.addWindowListener(new WindowAdapter {
+      override def windowClosing(e: WindowEvent): Unit = latch.countDown()
+      override def windowClosed(e: WindowEvent): Unit = latch.countDown()
+    })
+    gui.visible = true
   }
+
+  uiMode match
+    case "gui" =>
+      latch.await()
+
+    case _ =>
+      val tui = new Tui(controller)
+      controller.addObserver(tui)
+
+      given ExecutionContext = ExecutionContext.global
+      Future { tui.run() }
+
+      latch.await()
